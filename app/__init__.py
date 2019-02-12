942f07cea42adeef87d48e7daf82382c70952ecd
kelvinadeu <kelvinadeu25@gmail.com>
2019-02-13 00:48:45 +0300

    add to templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,44 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+#from flask_simplemde import SimpleMDE
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+#simple = SimpleMDE()
+mail = Mail()
+photos = UploadSet('photos', IMAGES)
+
+
+login_manager = LoginManager()
 
 def create_app(config_name):
+
     app = Flask(__name__)
-# Creating the app configurations
+
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    #Initializingflaskextensions
+    # configure UploadSet
+    configure_uploads(app, photos)
+
+    # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    #simple.init_app(app)
+    mail.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
+
     return app
