408b36b06428c479fa80554bbfb6b32d1699ef63
kelvinadeu <kelvinadeu25@gmail.com>
2019-02-15 09:31:59 +0300

    deployment

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
  #Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 migrate = Migrate(app,db)
 manager = Manager(app)
